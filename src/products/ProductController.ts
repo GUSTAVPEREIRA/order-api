673dc6849ebb95f266a3ba96ad45ae5992a42bf9
pereirovisk <gugupereira123@hotmail.com>
2021-11-28 21:27:49 -0300

    added swagger

--- src/products/ProductController.ts
+++ src/products/ProductController.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Inject, Post, Put } from "@nestjs/common";
+import { Body, Controller, Inject, Post, Put, Req } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { IProductRepository } from "./IProductRepository";
 import { NewProductRequest } from "./models/NewProductRequest";
 
+@ApiTags('products')
 @Controller('product')
 export class ProductController {
 
     constructor(@Inject('IProductRepository') private readonly productRepository: IProductRepository) { }
 
-
     @Post('')
-    async create(@Body() newProduct : NewProductRequest) {
+    @ApiOperation({ summary: 'Create product' })
+    @ApiResponse({ status: 500, description: 'Internal server Error' })
+    @ApiResponse({ status: 200 })
+    async create(@Req() @Body() newProduct: NewProductRequest) {
         return await this.productRepository.create(newProduct);
     }
 }
--- src/products/models/NewProductRequest.ts
+++ src/products/models/NewProductRequest.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Decimal } from "@prisma/client/runtime"
 
 export class NewProductRequest {
+    @ApiProperty({description: 'Product name', example: 'Book Clean Architecture'})    
     name: string;
+    @ApiProperty({description: 'Product price', example: '55.70'})
     price: Decimal;
 }
